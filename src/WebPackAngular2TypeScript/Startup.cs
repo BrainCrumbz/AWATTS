e99df3aac613745e122ecdd0c09f2f91541a4812
Giuseppe Piscopo <g.piscopo@braincrumbz.com>
2016-03-18 19:59:21 +0100

    Serve client built assets from build output directory

--- src/WebPackAngular2TypeScript/Startup.cs
+++ src/WebPackAngular2TypeScript/Startup.cs
@@ -5,8 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNet.StaticFiles;
+using Microsoft.AspNet.FileProviders;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
+using System.IO;
 /*
 using Newtonsoft.Json.Serialization;
 */
@@ -30,16 +33,16 @@ namespace WebPackAngular2TypeScript
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, IApplicationEnvironment appEnvironment)
         {
             app.UseStaticFiles();
 
-            /*
+            string clientBuildOutputPath = Path.Combine(appEnvironment.ApplicationBasePath, "buildOutput");
+
             app.UseStaticFiles(new StaticFileOptions()
             {
-                
+                FileProvider = new PhysicalFileProvider(clientBuildOutputPath),
             });
-            */
 
             app.UseMvc(config =>
             {
