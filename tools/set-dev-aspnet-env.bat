1a76ad4330704057e26e4d25335c3a290faa6446
Giuseppe Piscopo <g.piscopo@braincrumbz.com>
2016-10-17 15:57:51 +0200

    feat(tools): set ASP.NET Core development env with batch script

---
+++ tools/set-dev-aspnet-env.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+REM set-dev-aspnet-env.bat 
+REM Sets ASP.NET Core environment to development in current command line session.
+REM Usage: set-dev-aspnet-env
+
+SET ASPNETCORE_ENVIRONMENT=Development
+
+ECHO.
+ECHO Development environment set.
